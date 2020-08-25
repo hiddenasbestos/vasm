a179e1a0d3896c01a13ff7c28ceadc466b4e20f7
David Walters <contact@davidwaltersdevelopment.com>
2020-08-25 10:02:07 +0100

    -zx-tap option for BIN output

--- output_bin.c
+++ output_bin.c
@@ -8,7 +8,10 @@ static char *copyright="vasm binary output module 1.8a (c) 2002-2009,2013,2015,2
 
 #define BINFMT_RAW      0
 #define BINFMT_CBMPRG   1   /* Commodore VIC-20/C-64 PRG format */
+#define BINFMT_ZXTAP    2   /* ZX Spectrum .TAP format */
 static int binfmt = BINFMT_RAW;
+
+static uint8_t g_zxtap_name[ 10 ];
 
 static uint8_t g_gap_value = 0xFF;
 
@@ -172,11 +175,10 @@ static void write_output(FILE *f,section *sec,symbol *sym)
   }
 
 	/* map */
-	if ( g_print_map )
 	{
 		wpc = 0;
 
-		printf("\n ==== output section map ============\n\n");
+		if ( g_print_map ) printf("\n ==== output section map ============\n\n");
 		for ( i = 0; i < nsecs; ++i )
 		{
 			s = seclist[i];
@@ -186,39 +188,42 @@ static void write_output(FILE *f,section *sec,symbol *sym)
 			{
   				utaddr gap_size;
   				gap_size = (s->out_pos-1) - wpc;
-				
-				printf(" %08llX - %08llX %16s (%llu)\t\t<-- fill: 0x%02X\n",
+
+				if ( g_print_map ) printf(" %08llX - %08llX %16s (%llu)\t\t<-- fill: 0x%02X\n",
 					wpc,s->out_pos-1,"---",gap_size,g_gap_value);
 			}
 
-			if ( size == 1 ) {
-				printf(" %08llX            %16s",
-					s->out_pos,
-					s->name);
-			} else if ( ULLTADDR(size) > 0 ) {
-				printf(" %08llX - %08llX %16s (%llu)",
-					s->out_pos,s->out_pos + size-1,
-					s->name,ULLTADDR(size));
-			}
-
-			if ( ULLTADDR(s->out_pos) != ULLTADDR(s->org) )
+			if ( g_print_map )
 			{
-				if ( size == 1 )
-				{
-					printf("\t\t<-- local: %08llX",
-						ULLTADDR(s->org)
-					);
+				if ( size == 1 ) {
+					printf(" %08llX            %16s",
+						s->out_pos,
+						s->name);
+				} else if ( ULLTADDR(size) > 0 ) {
+					printf(" %08llX - %08llX %16s (%llu)",
+						s->out_pos,s->out_pos + size-1,
+						s->name,ULLTADDR(size));
 				}
-				else
-				{
-					printf("\t<-- local: %08llX - %08llX",
-						ULLTADDR(s->org),ULLTADDR(s->org)+size-1
-					);
-				}
-			}
 
-			if ( ULLTADDR(size) > 0 )
-				putchar('\n');
+				if ( ULLTADDR(s->out_pos) != ULLTADDR(s->org) )
+				{
+					if ( size == 1 )
+					{
+						printf("\t\t<-- local: %08llX",
+							ULLTADDR(s->org)
+						);
+					}
+					else
+					{
+						printf("\t<-- local: %08llX - %08llX",
+							ULLTADDR(s->org),ULLTADDR(s->org)+size-1
+						);
+					}
+				}
+
+				if ( ULLTADDR(size) > 0 )
+					putchar('\n');
+			}
 
 			wpc = s->out_pos + ULLTADDR(size);
 		}
@@ -228,15 +233,71 @@ static void write_output(FILE *f,section *sec,symbol *sym)
 			utaddr gap_size;
 			gap_size = (g_length-1) - wpc;
 
-			printf(" %08llX - %08llX %16s (%llu)\t\t<-- fill: 0x%02X\n",
-				wpc,g_length-1,"---",gap_size,g_gap_value);
+			if ( g_print_map )
+			{
+				printf(" %08llX - %08llX %16s (%llu)\t\t<-- fill: 0x%02X\n",
+					wpc,g_length-1,"---",gap_size,g_gap_value);
+			}
 
 			wpc = g_length;
 		}
 
-		printf( "\n  total: %12llu bytes.", wpc );
-		printf("\n ====================================\n\n");
+		if ( g_print_map )
+		{
+			printf( "\n  total: %12llu bytes.", wpc );
+			printf("\n ====================================\n\n");
+		}
 	}
+
+	if ( binfmt == BINFMT_ZXTAP )
+	{
+		uint64_t wpc2;
+		uint8_t zxtap_header[ 21 ];
+		int count;
+		char c;
+		uint8_t checkbit;
+
+		printf("\nWriting ZX Spectrum .TAP header (org = %d; length = %d)\n\n", sec->org, wpc );
+
+		/* ZX Spectrum TAP header */
+		zxtap_header[ 0 ] = 19;
+		zxtap_header[ 1 ] = 0;
+		zxtap_header[ 2 ] = 0;
+		zxtap_header[ 3 ] = 3;
+
+		/* NEXT 10 BYTES ARE FILENAME */
+		for(count=0;count<10;count++)
+		{
+			c = g_zxtap_name[ count ];
+			zxtap_header[ 4 + count ] = ( c < 32 ) ? 32 : c;
+		}
+
+		/* CODE DATA */
+		zxtap_header[ 14 ] = wpc&0xff;
+		zxtap_header[ 15 ] = (wpc>>8)&0xff;
+		zxtap_header[ 16 ] = sec->org&0xff;
+		zxtap_header[ 17 ] = (sec->org>>8)&0xff;
+		zxtap_header[ 18 ] = 0;
+		zxtap_header[ 19 ] = 0x80;
+
+		/* CHECK BIT */
+		for(count=2;count<20;count++)
+			checkbit=(checkbit ^ zxtap_header[count]);
+		zxtap_header[ 20 ]=checkbit;
+
+		for(count=0;count<21;++count)
+			fw8(f,zxtap_header[count]);
+
+		/* DATA HEADER */
+		wpc2 = wpc + 2; // data length + 2 checksums
+		zxtap_header[ 0 ] = wpc2&0xff;
+		zxtap_header[ 1 ] = (wpc2>>8)&0xff;
+		zxtap_header[ 2 ] = 255;
+		for(count=0;count<3;++count)
+			fw8(f,zxtap_header[count]);
+	}
+
+  uint8_t checkbit = 255;
 
   /* flush sections */
   pc = 0; wpc = 0;
@@ -245,7 +306,10 @@ static void write_output(FILE *f,section *sec,symbol *sym)
     if (s->out_pos > wpc) {
       /* fill gap between sections */
       for (; wpc < s->out_pos; ++wpc)
+      {
         fw8(f,g_gap_value);
+        checkbit=(checkbit ^ g_gap_value);
+	  }
     }
 
     for (p=s->first; p; p=p->next) {
@@ -253,7 +317,11 @@ static void write_output(FILE *f,section *sec,symbol *sym)
       npc = fwpcalign(f,p,s,pc);
       if (p->type == DATA) {
         for (i=0; i<p->content.db->size; i++)
-          fw8(f,(unsigned char)p->content.db->data[i]);
+        {
+		  uint8_t data = (unsigned char)p->content.db->data[i];
+          fw8(f,data);
+          checkbit=(checkbit ^ data);
+	  	}
       }
       else if (p->type == SPACE) {
         fwsblock(f,p->content.sb);
@@ -268,7 +336,13 @@ static void write_output(FILE *f,section *sec,symbol *sym)
   /* pad to given length? */
   for (; wpc<g_length; ++wpc) {
 	  fw8(f,g_gap_value);
+	  checkbit=(checkbit ^ g_gap_value);
   }
+
+	if ( binfmt == BINFMT_ZXTAP )
+	{
+		fw8(f,checkbit);
+	}
 }
 
 
@@ -276,6 +350,11 @@ static int output_args(char *p)
 {
   if (!strcmp(p,"-cbm-prg")) {
     binfmt = BINFMT_CBMPRG;
+    return 1;
+  }
+  else if (!strncmp(p,"-zx-tap=",8)) {
+    binfmt = BINFMT_ZXTAP;
+    strncpy( g_zxtap_name, p+8, 10 );
     return 1;
   }
   else if (!strcmp(p,"-rom")) {
